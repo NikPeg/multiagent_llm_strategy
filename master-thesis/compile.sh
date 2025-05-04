3d3b21e8684731b92f3d42ce160d3471c9b90e22
NikPeg <peganov.nik@gmail.com>
2025-05-04 16:27:45 +0300

    bib

--- master-thesis/compile.sh
+++ master-thesis/compile.sh
@@ -1,5 +1,5 @@
 pdflatex --shell-escape --interaction=nonstopmode main
 biber main
-#pdflatex --shell-escape --interaction=nonstopmode main
-#pdflatex --shell-escape --interaction=nonstopmode main
+pdflatex --shell-escape --interaction=nonstopmode main
+pdflatex --shell-escape --interaction=nonstopmode main
 open main.pdf
