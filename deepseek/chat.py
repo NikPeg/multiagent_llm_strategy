8f9ce58d3ee7d75035be004099f1f65f39871f98
NikPeg <peganov.nik@gmail.com>
2025-05-19 21:27:52 +0000

    chat

--- deepseek/chat.py
+++ deepseek/chat.py
@@ -30,7 +30,7 @@ while True:
     inputs = tokenizer(context, return_tensors="pt").to(model.device)
     outputs = model.generate(
         **inputs,
-        max_new_tokens=1000,
+        max_new_tokens=512,
         do_sample=True,
         temperature=0.7,
         top_p=0.95,
